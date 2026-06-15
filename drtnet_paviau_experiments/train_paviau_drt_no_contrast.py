d8ed4250e6ebd379c689836c266f64b7137deb93
daeyu666 <384114681@qq.com>
2026-06-15 19:01:03 +0800

    Add no-contrast DRTNet PaviaU experiment script

---
+++ drtnet_paviau_experiments/train_paviau_drt_no_contrast.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+import sys
+
+from common_drtnet_runner import run_experiment
+
+
+if __name__ == "__main__":
+    run_experiment(
+        "03_drt_no_contrast",
+        arch="MCT_rectangle",
+        disable_contrast=True,
+        extra_argv=sys.argv[1:],
+    )
