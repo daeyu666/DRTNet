81d19a7284f855ade998fa25007838749cfa28de
daeyu666 <384114681@qq.com>
2026-06-15 19:15:27 +0800

    Run single-scale PaviaU experiment with DRTnet

--- drtnet_paviau_experiments/train_paviau_drt_safa_single_scale.py
+++ drtnet_paviau_experiments/train_paviau_drt_safa_single_scale.py
@@ -8,7 +8,7 @@ from common_drtnet_runner import run_experiment
 if __name__ == "__main__":
     run_experiment(
         "02_drt_safa_single_scale",
-        arch="MCT",
+        arch="DRTnet",
         single_scale_safa=True,
         extra_argv=sys.argv[1:],
     )
