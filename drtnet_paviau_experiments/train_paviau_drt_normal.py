cf1171fdcda48873a803fd1a64090699302e362a
daeyu666 <384114681@qq.com>
2026-06-15 19:07:24 +0800

    Use MCT architecture for normal PaviaU experiment

--- drtnet_paviau_experiments/train_paviau_drt_normal.py
+++ drtnet_paviau_experiments/train_paviau_drt_normal.py
@@ -8,6 +8,6 @@ from common_drtnet_runner import run_experiment
 if __name__ == "__main__":
     run_experiment(
         "01_drt_normal",
-        arch="MCT_rectangle",
+        arch="MCT",
         extra_argv=sys.argv[1:],
     )
