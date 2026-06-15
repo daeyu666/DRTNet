ba0a0a7f5c75f1b9d85247cd5f04c92857cd4296
daeyu666 <384114681@qq.com>
2026-06-15 19:00:44 +0800

    Add normal DRTNet PaviaU experiment script

---
+++ drtnet_paviau_experiments/train_paviau_drt_normal.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+import sys
+
+from common_drtnet_runner import run_experiment
+
+
+if __name__ == "__main__":
+    run_experiment(
+        "01_drt_normal",
+        arch="MCT_rectangle",
+        extra_argv=sys.argv[1:],
+    )
