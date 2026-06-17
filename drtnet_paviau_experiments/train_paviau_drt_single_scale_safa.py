59614f1aa0b299a592cb2f73c8f5a458fc29cf22
daeyu666 <384114681@qq.com>
2026-06-17 09:16:22 +0800

    Use separate output for no-downsample DRT ablation

--- drtnet_paviau_experiments/train_paviau_drt_single_scale_safa.py
+++ drtnet_paviau_experiments/train_paviau_drt_single_scale_safa.py
@@ -7,7 +7,7 @@ from common_drtnet_runner import run_experiment
 
 if __name__ == "__main__":
     run_experiment(
-        "02_drt_single_scale_safa",
+        "02_drt_single_scale_no_downsample",
         arch="DRTnet",
         single_scale_safa=True,
         extra_argv=sys.argv[1:],
