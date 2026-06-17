5bbf660e8cb288a41333191c247fde3a1173322d
daeyu666 <384114681@qq.com>
2026-06-17 09:15:57 +0800

    Add corrected single-scale DRT experiment script

---
+++ drtnet_paviau_experiments/train_paviau_drt_single_scale_safa.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+import sys
+
+from common_drtnet_runner import run_experiment
+
+
+if __name__ == "__main__":
+    run_experiment(
+        "02_drt_single_scale_safa",
+        arch="DRTnet",
+        single_scale_safa=True,
+        extra_argv=sys.argv[1:],
+    )
